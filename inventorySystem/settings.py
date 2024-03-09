53fb09a06f7ca2be497567cdc724be6caa99cee0
Taha Yasir Yesil <tahayesil4040@gmail.com>
2024-03-10 02:55:13 +0300

    commit

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dkbt!_d=a+))h0*ga5jxy7#p$-_^pxpmzpz@d=1cq!7m(8b7a5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
