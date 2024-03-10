ee81e001eb812fee74efe632b28407017e7fa04f
Taha Yasir Yesil <tahayesil4040@gmail.com>
2024-03-10 03:58:25 +0300

    Update settings.py

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIR = (
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,4 +139,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "/inventory"
 LOGIN_URL = "login"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
